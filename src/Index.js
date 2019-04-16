edde94cc43e744a25b6b9e9e1bb8ed6b8f78ebb9
moranja <jeffadammoran@gmail.com>
2019-04-16 14:50:19 -0500

    buttons

---
+++ src/Index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const h2 = document.createElement('h2')
+  h2.textContent = "Design a new Layout"
+  h2.addEventListener('click', function() {
+    loadDesignPage()
+  })
+  const body = document.querySelector('body')
+  body.append(h2)
+})
