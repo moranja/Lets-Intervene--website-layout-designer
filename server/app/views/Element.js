ce0e2ea5dcd1d5fec58955e65c9cad6bea4c7364
moranja <jeffadammoran@gmail.com>
2019-04-15 22:40:24 -0500

    full element movement

--- server/app/views/Element.js
+++ server/app/views/Element.js
@@ -5,32 +5,57 @@ class Element {
     this.rightEdge = 198
     this.bottomEdge = 100
     this.topEdge = 198
-    this.borderWidth = 20
+    this.borderWidth = 30
     this.renderAll()
 
-
+    this.leftEdgeIsMoving = false
+    this.bottomEdgeIsMoving = false
     this.rightEdgeIsMoving = false
+    this.topEdgeIsMoving = false
+
     document.addEventListener('mousemove', (e) => {
-      if (this.rightEdgeIsMoving) {
-        this.rightEdge = e.clientX
-        this.renderAll()
-        this.body.innerHTML = ""
-        this.appendAllToBody()
+      if (this.leftEdgeIsMoving) {
+        if (e.clientX > this.rightEdge - this.borderWidth) {
+          this.leftEdge = this.rightEdge - this.borderWidth
+        } else {
+          this.leftEdge = e.clientX
+        }
+        this.reset()
+      } else if (this.bottomEdgeIsMoving) {
+        if (800 - e.clientY > this.topEdge - this.borderWidth) {
+          this.bottomEdge = this.topEdge - this.borderWidth
+        } else {
+          this.bottomEdge = 800 - e.clientY
+        }
+        this.reset()
+      } else if (this.rightEdgeIsMoving) {
+        if (e.clientX < this.leftEdge + this.borderWidth) {
+          this.rightEdge = this.leftEdge + this.borderWidth
+        } else {
+          this.rightEdge = e.clientX
+        }
+        this.reset()
+      } else if (this.topEdgeIsMoving) {
+        if (800 - e.clientY < this.bottomEdge + this.borderWidth) {
+          this.topEdge = this.bottomEdge + this.borderWidth
+        } else {
+          this.topEdge = 800 - e.clientY
+        }
+        this.reset()
+      } else if (this.interiorIsMoving) {
+        this.leftEdge = e.clientX - this.leftDistance
+        this.bottomEdge = 800-e.clientY - this.bottomDistance
+        this.rightEdge = e.clientX + this.rightDistance
+        this.topEdge = 800-e.clientY + this.topDistance
+        this.reset()
       }
     })
     document.addEventListener('mouseup', () => {
+      this.leftEdgeIsMoving = false
+      this.bottomEdgeIsMoving = false
       this.rightEdgeIsMoving = false
-    })
-
-
-    this.leftSide.addEventListener('click', function() {
-      console.log('left')
-    })
-    this.bottomSide.addEventListener('click', function() {
-      console.log('bottom')
-    })
-    this.topSide.addEventListener('click', function() {
-      console.log('top')
+      this.topEdgeIsMoving = false
+      this.interiorIsMoving = false
     })
   }
 
@@ -41,6 +66,20 @@ class Element {
       this.leftEdge,
       this.bottomEdge
     )
+    this.leftSide.addEventListener('mousedown', (e) => {
+      e.preventDefault()
+      this.leftEdgeIsMoving = true
+    })
+    this.bottomSide = this.createDiv(
+      this.borderWidth,
+      this.rightEdge-this.leftEdge+this.borderWidth,
+      this.leftEdge,
+      this.bottomEdge
+    )
+    this.bottomSide.addEventListener('mousedown', (e) => {
+      e.preventDefault()
+      this.bottomEdgeIsMoving = true
+    })
     this.rightSide = this.createDiv(
       this.topEdge-this.bottomEdge+this.borderWidth,
       this.borderWidth,
@@ -51,38 +90,53 @@ class Element {
       e.preventDefault()
       this.rightEdgeIsMoving = true
     })
-    this.bottomSide = this.createDiv(
-      this.borderWidth,
-      this.rightEdge-this.leftEdge+this.borderWidth,
-      this.leftEdge,
-      this.bottomEdge
-    )
     this.topSide = this.createDiv(
       this.borderWidth,
       this.rightEdge-this.leftEdge+this.borderWidth,
       this.leftEdge,
       this.topEdge
     )
+    this.topSide.addEventListener('mousedown', (e) => {
+      e.preventDefault()
+      this.topEdgeIsMoving = true
+    })
+    this.interior = this.createDiv(
+      this.topEdge-this.bottomEdge-this.borderWidth,
+      this.rightEdge-this.leftEdge-this.borderWidth,
+      this.leftEdge+this.borderWidth,
+      this.bottomEdge+this.borderWidth
+    )
+    this.interior.style.background = "blue"
+    this.interior.addEventListener('mousedown', (e) => {
+      e.preventDefault()
+      this.interiorIsMoving = true
+      this.leftDistance = e.clientX-this.leftEdge
+      this.bottomDistance = (800-e.clientY)-this.bottomEdge
+      this.rightDistance = this.rightEdge-e.clientX
+      this.topDistance = this.topEdge-(800-e.clientY)
+    })
   }
 
-  serveLeft() {
-    return this.leftSide
-  }
-  serveBottom() {
-    return this.bottomSide
-  }
-  serveRight() {
-    return this.rightSide
-  }
-  serveTop() {
-    return this.topSide
+  removeAll() {
+    this.leftSide.remove()
+    this.bottomSide.remove()
+    this.rightSide.remove()
+    this.topSide.remove()
+    this.interior.remove()
   }
 
   appendAllToBody() {
-    this.body.append(this.serveLeft())
-    this.body.append(this.serveBottom())
-    this.body.append(this.serveRight())
-    this.body.append(this.serveTop())
+    this.body.append(this.leftSide)
+    this.body.append(this.bottomSide)
+    this.body.append(this.rightSide)
+    this.body.append(this.topSide)
+    this.body.append(this.interior)
+  }
+
+  reset() {
+    this.removeAll()
+    this.renderAll()
+    this.appendAllToBody()
   }
 
   createDiv(height, width, left, bottom) {
