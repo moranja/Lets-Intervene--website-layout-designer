d084784236eb4adcd7a4ab04a6193c1ad2adace9
github username <github email address>
2019-04-18 16:39:34 -0500

    your page working

--- server/app/controllers/layouts_controller.rb
+++ server/app/controllers/layouts_controller.rb
@@ -1,25 +1,25 @@
 class LayoutsController < ApplicationController
-    def index
-        render json: Layout.all,
-            include: {user: {except: [:id, :created_at, :updated_at]}}
-    end
+  def index
+      render json: Layout.all,
+          include: {user: {except: [:id, :created_at, :updated_at]}}
+  end
 
-    def create
-      layout = Layout.create(layout_attributes)
-      render json: layout
-    end
+  def create
+    layout = Layout.create(layout_attributes)
+    render json: layout
+  end
 
-    def show
-        render json: Layout.find(params[:id])
-    end
+  def show
+      render json: Layout.find(params[:id])
+  end
 
-    def edit
-        render json: Layout.find(params[:id])
-    end
+  def edit
+      render json: Layout.find(params[:id])
+  end
 
-    private
+  private
 
-      def layout_attributes
-        params.permit(:name, :user_id, :img, :html)
-      end
+  def layout_attributes
+    params.permit(:name, :user_id, :img, :html)
+  end
 end
